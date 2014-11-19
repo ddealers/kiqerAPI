bd2a2dcdc7d899f6a1ccb2076af17e07bedc58d4
vampaynani <vampay@digitaldealers.mx>
2014-11-19 15:49:17 -0600

    base code

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ActiveRecord::Base
 				uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, length: {minimum: 6}
+	has_one :profile
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118072230) do
+ActiveRecord::Schema.define(version: 20141119214313) do
+
+  create_table "profiles", force: true do |t|
+    t.string   "name"
+    t.string   "surname"
+    t.datetime "birth"
+    t.string   "country"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name",            null: false
