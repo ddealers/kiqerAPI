5060fcc0dc4be9a45f36db5ea70db0fc47ed07ba
Juan Francisco <fherrera@softin.mx>
2014-11-26 17:37:10 -0600

    API user, profile. kiqs, posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+	before_filter :cors_preflight_check
+	after_filter :cors_set_access_control_headers
+
+	def cors_preflight_check
+		if request.method == :options
+			headers['Access-Control-Allow-Origin'] = '*'
+			headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+			headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+			headers['Access-Control-Max-Age'] = '1728000'
+			render :text => '', :content_type => 'text/plain'
+	  end
+	end
+
+	def cors_set_access_control_headers
+		headers['Access-Control-Allow-Origin'] = '*'
+		headers['Access-Control-Allow-Headers'] = 'X-AUTH-TOKEN, X-API-VERSION, X-Requested-With, Content-Type, Accept, Origin'
+		headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+		headers['Access-Control-Max-Age'] = "1728000"
+	end
 end
